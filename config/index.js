69abb16b9fafbad895a48fbd8397430ff642960e
Darwin <153224587+R4yli@users.noreply.github.com>
2024-06-13 21:09:02 -0500

    Update index.js

--- config/index.js
+++ config/index.js
@@ -3,7 +3,7 @@ const { Sequelize } = require("sequelize");
 const database = "railway";
 const username = "postgres";
 const password = process.env.DATABASE_PASSWORD;
-const host = "roundhouse.proxy.rlwy.net";
+const host = "viaduct.proxy.rlwy.net";
 const port = "32432";
 
 const sequelize = new Sequelize(database, username, password, {
